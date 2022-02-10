00e54f903b7e64d47654c010a1bd31a1bf7d23f2
to-sho-volodya <keklolorbedol13@gmail.com>
2022-02-10 19:13:27 +0200

    first commit

---
+++ 4th-layout/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+  $('.programs').slick({
+
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    infinite: false,
+    arrows: false,
+    dots:true,
+    // fade: true,
+    // adaptiveHeight:true,
+    // draggable:true,
+  });
+});
+
+$(document).ready(function () {
+  $('.quote').slick({
+
+    autoplay: true,
+    autoplaySpeed: 10000,
+    infinite: true,
+    arrows: false,
+    dots:true,
+    // fade: true,
+    responsive: [{
+      breakpoint: 0,
+      settings: {
+  }
+  }
+    ]
+     // adaptiveHeight:true,
+     // draggable:false,
+  });
+});
+
+$(document).ready(function () {
+  $('.mentors_box').slick({
+
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    infinite: false,
+    arrows: false,
+    dots:true,
+    // fade: true,
+    // adaptiveHeight:true,
+    // draggable:true,
+  });
+});
